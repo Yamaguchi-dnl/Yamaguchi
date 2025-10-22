8d44c5e3276d81d1a2eec37106d78bbc7dcd2d75
Daniel Yamaguchi <danielyamaguchi409@gmail.com>
2025-10-22 18:03:26 +0000

    deixe maior (_for element p_)

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -6,7 +6,7 @@ export default function HeroSection() {
         <h1 className="text-5xl sm:text-7xl md:text-8xl lg:text-[110px] font-semibold tracking-tighter uppercase animate-fade-in-down">
           DANIEL YAMAGUCHI
         </h1>
-        <p className="mt-8 text-sm text-white/80 animate-fade-in-up [animation-delay:200ms]">
+        <p className="mt-8 text-base text-white/80 animate-fade-in-up [animation-delay:200ms]">
           WEB DESIGNER & ESTRATEGISTA DIGITAL
         </p>
       </div>
